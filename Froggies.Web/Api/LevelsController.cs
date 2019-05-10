5f215f2c97d14cc53963d4b888e5acd247eb07f3
Shifu462 <gunka462@gmail.com>
2019-05-10 10:57:12 +0300

    Fix #10: правки по контроллеру.

--- Froggies.Web/Api/LevelsController.cs
+++ Froggies.Web/Api/LevelsController.cs
@@ -15,11 +15,20 @@ namespace Froggies.Web.Api
         }
 
         [Route("ids")]
-        public int[] GetLevelIds()
+        public IActionResult GetLevelIds()
         {
-            return this.Context.Levels.Select(x => x.Id).ToArray();
+            return this.Ok(this.Context.Levels.Select(x => x.Id).ToArray());
         }
 
+        [Route("{id:int}")]
+        public IActionResult LoadLevel(int id)
+        {
+            var lvl = this.Context.Levels.FirstOrDefault(x => x.Id == id);
 
+            if (lvl == null)
+                return this.BadRequest("Level not found.");
+
+            return this.Ok(lvl.CellsJson);
+        }
     }
 }
